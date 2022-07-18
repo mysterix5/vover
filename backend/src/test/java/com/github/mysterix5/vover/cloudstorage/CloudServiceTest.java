d0a2f0435037965e3b369c38a42631fe6c67664c
Lukas <lukasduechting@mailbox.org>
2022-07-18 08:45:02 +0200

    i can read mp3 to AudioInputStream now

--- backend/src/test/java/com/github/mysterix5/vover/cloudstorage/CloudServiceTest.java
+++ backend/src/test/java/com/github/mysterix5/vover/cloudstorage/CloudServiceTest.java
@@ -5,8 +5,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.*;
+import java.net.URL;
 import java.util.List;
+import java.io.File;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -35,5 +43,24 @@ class CloudServiceTest {
         assertThat(returnVal.getContentLength()).isEqualTo(344064);
         assertThat(returnVal.getContentType()).isEqualTo("audio/x-wav");
         assertThat(returnVal.getData()).isEqualTo(obDasBytes);
+    }
+    @Test
+    void loadMultipleAudioFromCloudAndMergeMp3() throws IOException, UnsupportedAudioFileException {
+        CloudRepository cloudRepository = Mockito.mock(CloudRepository.class);
+        CloudService cloudService = new CloudService(cloudRepository);
+
+        String filePath = "/Users/lukas/Downloads/bei.mp3";
+        File file = new File(filePath);
+//        var fileInputStream = new FileInputStream(new File(filePath));
+//        var bytes = fileInputStream.readAllBytes();
+//        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+//
+        AudioFileFormat format = AudioSystem.getAudioFileFormat(file);
+        System.out.println(format);
+//        AudioInputStream in = AudioSystem.getAudioInputStream(bytes);
+
+//        AudioFileFormat audioFileFormat = AudioSystem.getAudioFileFormat(file);
+//        System.out.println(audioFileFormat);
+
     }
 }
